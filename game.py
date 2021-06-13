5cb64ea5962c6dabe4d50e9bce86ed86993de4f3
ZhuohaoTan <80125456+ZhuohaoTan@users.noreply.github.com>
2021-06-13 16:34:36 -0400

    Add files via upload

---
+++ game.py
@@ -0,0 +1,61 @@
+
+
+world = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+
+def count(w,r,c):
+    
+    dir=[[-1,0],[-1,1],[-1,-1],[1,0],[1,-1],[1,1],[0,1],[0,-1]]
+    count = 0
+
+    for i in dir:
+        raw = r+int(i[0])
+        col = c+int(i[1])
+        try:
+
+            if bool(w[raw][col]):
+                count += 1
+    
+        except IndexError:
+            continue
+
+    return count
+
+x = int(input("Enter the nubmer of weeks: " ))
+
+for XD in range(0,x):
+    die = []
+    live = []
+    for raw in range(len(world)):
+        for i in range(len(world[raw])):
+            if bool(world[raw][i]):
+                if count(world,raw,i) < 2 or count(world,raw,i) > 3:
+                    die.append([raw,i])            
+            elif count(world,raw,i) == 3:
+                    live.append([raw,i])
+       
+    for i in die:
+        world[int(i[0])][int(i[1])] = 0
+
+    for i in live:
+        world[int(i[0])][int(i[1])] = 1
+
+'''update'''         
+x=""
+for raw in world:
+    for i in raw:
+        if bool(i):
+            x += "O"+" "
+        else:
+            x += "."+" "
+    x += "\n"
+print(x)
